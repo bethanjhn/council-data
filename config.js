fa63501a1b5272145f9ccbb2a59b9d1ec9677768
bethanjhn <46195797+bethanjhn@users.noreply.github.com>
2019-04-21 16:26:11 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+var config = {onsumer_key =          'E2A12LsF2i9gwpSQDxNO3MgDf',
+    consumer_secret =       'P5YnIsvOb4DPDibOQTpdiJBTvr2WfDaM6ReRjMWupZ7aPQvtBY',
+    access_token =          '1100868937816915968-0MpWKaemkyjtz1MjTL6AmCWglIodFV',
+    access_token_secret =   'QrVdCTaUGum3YsIMxcpwl499ELeb1cevhA9WsSO5Zl7gX'
+}
